772a7cd61a099117780ce11692f399d8baee96a7
Keita <184593592+ktsgsg@users.noreply.github.com>
2025-10-24 18:41:30 +0900

    Rxjavaを使って非同期でやった．Rxjavaはライセンスを確認すること．しないとリリースできないかもしれん

--- app/src/main/java/jp/ac/meijou/android/hannama/ai/gemini.java
+++ app/src/main/java/jp/ac/meijou/android/hannama/ai/gemini.java
@@ -9,22 +9,24 @@ import com.google.genai.types.GenerateContentResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import jp.ac.meijou.android.hannama.BuildConfig;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 // AI Gemini APIを使うためのクラス
 public class gemini {
-
-    private static final Logger log = LoggerFactory.getLogger(gemini.class);
-
-    public static void GEMgetcontent() {
+    public static String GEMgetcontent() {
         Client client = Client.builder().apiKey(BuildConfig.GOOGLE_API_KEY).build();
-
         GenerateContentResponse response =
                 client.models.generateContent(
                         "gemini-2.5-flash",
-                        "Explain how AI works in a few words",
+                        "ちんちんがかゆいーな",
                         null);
+        return response.text();
 
-        System.out.println(response.text());
     }
 }
